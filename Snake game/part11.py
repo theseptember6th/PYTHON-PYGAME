f9c5d425ddbced0abd8e88e700d14ab8f8913572
kristal <shresthakristal42@gmail.com>
2024-06-16 10:28:29 +0545

    high score setup using file as backup

--- Snake game/part11.py
+++ Snake game/part11.py
@@ -1,7 +1,7 @@
 """
 AUTHOR: KRISTAL SHRESTHA
 DATE: 6/15/2024
-PURPOSE: Snake game part 10 (highscore setup using file as backup)
+PURPOSE: Snake game part 11 (highscore setup using file as backup)
 """
 
 import pygame
